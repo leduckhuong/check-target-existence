7e8c3c91ed48a42c7da99355d8d04230f25a668b
leduckhuong <leduckhuong@kali.kali.org>
2024-09-27 11:10:32 +0700

    change file check controller

--- src/app/controllers/CheckTargetExistence.controller.js
+++ src/app/controllers/CheckTargetExistence.controller.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 class CheckTargetExistence {
-    home (req, res) {
+    async home (req, res) {
         const target = req.body.target;
         if (!req.file) {
             return res.status(400).send('No file uploaded.');
@@ -18,46 +18,34 @@ class CheckTargetExistence {
             }
             headers[item[0]] = item[1];
         }
-        Array.from(pathArr).forEach( async path => {
-            const uri = target.trim() + path.trim();
+        const result = [];
+        for (const path of pathArr) {
+            const uri = target.trim() + '/' + path.trim();
             try {
-                const response = await axios.get(uri, {headers});
+                const response = await axios.get(uri, { headers });
                 const data = response.data;
-                const encoder = new TextEncoder(); 
-                const byteArray = encoder.encode(data); 
-                const byteLength = byteArray.length; 
-                console.log(`Target=${target}${path}   status=${response.status}   Content-Length=${byteLength}`);
-            } catch (error) {
-                console.log('Error');
-            }
-        });
-        res.send('oce');
-    }
-    async test (req, res) {
-        const targets = ["http://hahah.hhaahh.com", "http://test.php.vulnweb.com", "https://ahanncn.com"];
-        targets.forEach(async target => {
-            try {
-                const response = await axios.get(target, {
-                    headers: {
-                        'Host': 'testphp.vulnweb.com',
-                        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                        'Accept-Language': 'en-US,en;q=0.7,vi;q=0.3',
-                        'Accept-Encoding': 'gzip, deflate, br',
-                        'Connection': 'keep-alive',
-                        'Upgrade-Insecure-Requests': '1'
-                    }
+                const encoder = new TextEncoder();
+                const byteArray = encoder.encode(data);
+                const byteLength = byteArray.length;
+                result.push({
+                    uri,
+                    byteLength,
+                    status: response.status
                 });
-                const data = response.data;
-                const encoder = new TextEncoder(); 
-                const byteArray = encoder.encode(data); 
-                const byteLength = byteArray.lzength; 
-                console.log(`Target=${target}   status=${response.status}   Content-Length=${byteLength}`);
             } catch (error) {
-                console.log('Error');
+                const byteLength = error.response ? new TextEncoder().encode(error.response.data).length : 0;
+                const status = error.response ? error.response.status : 500;
+                result.push({
+                    uri,
+                    byteLength,
+                    status
+                });
             }
-        });
-        res.send('oce');
+        }
+        return res.render('index', {
+            isResult: true,
+            result
+        })
     }
 }
 
--- src/routes/routers/checkTargetExistence.router.js
+++ src/routes/routers/checkTargetExistence.router.js
@@ -6,6 +6,5 @@ const upload = require('../../app/middlewares/upload');
 const checkTargetExistenceController = require('../../app/controllers/CheckTargetExistence.controller');
 
 router.post('/', upload.single('file'), checkTargetExistenceController.home);
-router.get('/', checkTargetExistenceController.test);
 
 module.exports = router;
